e7bd3a9506afada7dc8451a79e158f4965c27d62
peinorim <peinorim@gmail.com>
2020-03-30 15:29:21 +0200

    init repo

--- models/data.py
+++ models/data.py
@@ -9,4 +9,6 @@ class Data:
 
     def init_data(self):
         r = requests.get("https://peinorim.github.io/python_sandbox/data/covid-19/data.json")
+        if r.status_code != 200:
+            r = requests.get("https://raw.githubusercontent.com/peinorim/python_sandbox/master/data/covid-19/data.json")
         self.data = r.json()
