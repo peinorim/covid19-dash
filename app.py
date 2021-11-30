45484dd927f5b38be0d1d17c7fbdae949f4dc6fc
peinorim <peinorim@gmail.com>
2021-11-30 21:57:10 +0100

    updates

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import os
 from datetime import datetime
 
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html
+from dash import dcc
 
 import dash_bootstrap_components as dbc
 import redis
